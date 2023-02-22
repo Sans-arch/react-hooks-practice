533a776502181256b3ead445aa2cb61293e18c6b
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-22 13:25:39 -0300

    first version

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react';
+// useCallback = serve para memorização de funções
+import { useState, useCallback } from 'react';
 
 import './App.css'
 
+const fnCounter = new Set();
+
+// o método render() em functional components é a própria função, ou seja, é como se a função App()
+//  fosse redeclarada a cada render
 function App() {
-  const state = useState('Santiago');
+  const [counter, setCounter] = useState(0);
+
+  // Com [], criamos a instância 1 unica vez, então teremos somente 1 unica vez esta função criada na aplicação.
+  // Usamos useCallback quando precisamos passar uma função para um componente filho!
+  const handlePlus = useCallback(() => {
+    setCounter((prevState) => prevState + 1);
+  }, []);
 
   return (
     <div>
-      <h1>useState</h1>
+      <h1>{counter}</h1>
+      <Button onClick={handlePlus} />
     </div>
+  );
+}
+
+function Button(props) {
+  return (
+      <button onClick={props.onClick}>+</button>
   );
 }
 
