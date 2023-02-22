8707f2e5f3ff52d5f9d16197dc57ec57dbfab24a
Santiago Fleitas <sansfleitas@gmail.com>
2023-02-22 13:39:40 -0300

    use Memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 import './App.css'
 
 function App() {
-  const state = useState('Santiago');
+  const [counter, setCounter] = useState(1);
+  const [name, setName] = useState('');
+
+  // useMemo = retorna o resultado de uma função passada como argumento, porém só executara ela caso um valor de deps mude
+  const total = useMemo(() => {
+    console.log('useMemo rodou');
+    return (counter * 129389 * 1029301 * 10293012);
+  }, [counter]);
+
+
+  function handlePlus() {
+    setCounter((prevState) => prevState + 1);
+  }
 
   return (
     <div>
-      <h1>useState</h1>
+      <h1>{counter}</h1>
+      <h4>{total}</h4>
+      <button onClick={handlePlus}>+</button>
+
+      <br /><br />
+      <span>{name}</span><br />
+      <input onChange={e => setName(e.target.value)} />
     </div>
   );
 }
